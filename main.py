8b9a784eaa7cc1b6bcd122f223c470629e90ef26
palassaha <123sahap640@gmail.com>
2024-12-16 13:38:46 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from ML_Project import logger
+from src.ML_Project import logger
+from src.ML_Project.pipeline.data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/ML_Project/utils/common.py
+++ src/ML_Project/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from ML_Project import logger
+from src.ML_Project import logger
 import json
 import joblib
 from ensure import ensure_annotations
