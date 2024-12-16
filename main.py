28d581121528619186ad4cd6561495205ec068a2
palassaha <123sahap640@gmail.com>
2024-12-17 01:13:37 +0530

    model eval, mlflow

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.ML_Project.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from src.ML_Project.pipeline.data_validation import DataValidationTrainingPipeline
 from src.ML_Project.pipeline.data_transformation import DataTransformationTrainingPipeline
 from src.ML_Project.pipeline.model_trainer import ModelTrainerTrainingPipeline
+from src.ML_Project.pipeline.model_eval import ModelEvaluationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -39,6 +40,16 @@ STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
