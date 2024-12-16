d10a8519d8442d7e7c113e59d62a56072331aede
palassaha <123sahap640@gmail.com>
2024-12-16 20:54:49 +0530

    data validation

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from src.ML_Project import logger
 from src.ML_Project.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.ML_Project.pipeline.data_validation import DataValidationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
